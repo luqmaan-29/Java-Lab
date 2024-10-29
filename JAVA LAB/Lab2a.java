d5b2a94036718c86cb95196753ef4a2428de0f56
Luqmaan <syed.mohammedluqmaan@msam.christuniversity.in>
2024-10-29 12:05:52 +0530

    commit

--- JAVA LAB/Lab2a.java
+++ JAVA LAB/Lab2a.java
@@ -7,6 +7,7 @@ public class Lab2a {
     public static void findTopKFrequent(int K) {
         int n = numbers.length;
 
+        // Find the maximum number in the array
         int maxNumber = 0;
         for (int i = 0; i < n; i++) {
             if (numbers[i] > maxNumber) {
@@ -14,6 +15,7 @@ public class Lab2a {
             }
         }
 
+        // Create an array to store the frequency of each number
         int[] frequency = new int[maxNumber + 1];
         for (int i = 0; i < n; i++) {
             frequency[numbers[i]]++;
@@ -22,10 +24,12 @@ public class Lab2a {
         System.out.println("Top " + K + " frequent numbers:");
         int count = 0;
 
+        // Find the K most frequent numbers
         while (count < K) {
             int maxFreq = 0;
             int mostFrequent = -1;
 
+            // Find the number with the highest frequency and largest value in case of ties
             for (int i = 0; i <= maxNumber; i++) {
                 if (frequency[i] > maxFreq || (frequency[i] == maxFreq && i > mostFrequent)) {
                     maxFreq = frequency[i];
@@ -33,6 +37,7 @@ public class Lab2a {
                 }
             }
 
+            // Print the most frequent number and mark it as used
             if (mostFrequent != -1 && maxFreq > 0) {
                 System.out.print(mostFrequent + " ");
                 frequency[mostFrequent] = 0;
@@ -49,6 +54,15 @@ public class Lab2a {
         int N = sc.nextInt();
 
         numbers = new int[N];
+
+        System.out.println("Enter " + N + " numbers:");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the value of K: ");
+        int K = sc.nextInt();
+
+        findTopKFrequent(K);
     }
 }
-  
