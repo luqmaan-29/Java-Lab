acaad7c2f211d9ae4fdb8f2d91ca9650dbef80a4
Luqmaan <syed.mohammedluqmaan@msam.christuniversity.in>
2024-10-29 13:11:58 +0530

    commit

--- JAVA LAB/Lab2b.java
+++ JAVA LAB/Lab2b.java
@@ -3,29 +3,37 @@ public class Lab2b {
 
     public static void findMaxProfit(int[] prices) {
         int n = prices.length;
+
         if (n < 2) {
             maxProfit = 0;
             return;
         }
 
-        int[] profitLeft = new int[n];
-        int[] profitRight = new int[n];
-
         int minPrice = prices[0];
-        for (int i = 1; i < n; i++) {
-            minPrice = Math.min(minPrice, prices[i]);
-            profitLeft[i] = Math.max(profitLeft[i - 1], prices[i] - minPrice);
-        }
-
         int maxPrice = prices[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            maxPrice = Math.max(maxPrice, prices[i]);
-            profitRight[i] = Math.max(profitRight[i + 1], maxPrice - prices[i]);
+
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
         }
 
-        maxProfit = 0;
-        for (int i = 0; i < n; i++) {
-            maxProfit = Math.max(maxProfit, profitLeft[i] + profitRight[i]);
+        for (int i = n - 2; i >= 0; i--) {
+            if (prices[i] > maxPrice) {
+                maxPrice = prices[i];
+            }
+
+            int profit = maxPrice - prices[i];
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
         }
     }
 
@@ -33,6 +41,8 @@ public class Lab2b {
         int[] prices1 = {20, 30, 45, 60, 75, 70};
         findMaxProfit(prices1);
         System.out.println("Max Profit: " + maxProfit);
+
+        maxProfit = 0;
 
         int[] prices2 = {2, 30, 15, 10, 8, 25, 80};
         findMaxProfit(prices2);
