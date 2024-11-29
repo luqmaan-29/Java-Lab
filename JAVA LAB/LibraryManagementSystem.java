8fbd2af0dd12c7bd3a4bbffc5f1b8d14eecec9e6
Luqmaan <syed.mohammedluqmaan@msam.christuniversity.in>
2024-11-29 14:05:31 +0530

    commit

--- JAVA LAB/LibraryManagementSystem.java
+++ JAVA LAB/LibraryManagementSystem.java
@@ -16,23 +16,24 @@ class Book {
         return title;
     }
     public void borrowBook() {
-        if (isAvailable) {
+        if (isAvailable) 
+        {
             isAvailable = false;
               System.out.println("Book borrowed: " + title);
         } else {
-               System.out.println("Book is unavailable: " + title);
+                 System.out.println("Book is unavailable: " + title);
         }
     }
     public void returnBook() {
-        if (!isAvailable) {
+          if (!isAvailable) {
              isAvailable = true;
                 System.out.println("Book returned: " + title);
-        } else {
-            System.out.println("Book was not borrowed: " + title);
+        }   else {
+                    System.out.println("Book was not borrowed: " + title);
         }
     }
     public void displayInfo() {
-        System.out.println("Title: " + title + ", Author: " + author + ", Available: " + (isAvailable ? "Yes" : "No"));
+                System.out.println("Title: " + title + ", Author: " + author + ", Available: " + (isAvailable ? "Yes" : "No"));
     }
 }
 
@@ -77,20 +78,22 @@ public class LibraryManagementSystem {
         
         Book book1 = new FictionBook(101, "ghost diaries", "alan john", "horror");
         Book book2 = new ReferenceBook(201, "design of algorithms", "syed ali", 3);
+        Book book3 = new FictionBook(102,"horror comedies","vivek george","horror");
 
         
         System.out.println("Library Inventory:");
         book1.displayInfo();
         book2.displayInfo();
+        book3.displayInfo();
 
         
         System.out.println("\nBorrowing Books:");
-        book1.borrowBook(); 
-        book1.borrowBook(); 
+        book2.borrowBook(); 
+        book2.borrowBook(); 
 
         
         System.out.println("\nReturning Books:");
-        book1.returnBook();  
+        book2.returnBook(); 
 
 
         System.out.println("\nLibrary Final Stats:");
