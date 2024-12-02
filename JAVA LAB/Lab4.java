019638404e7ac064d17fbfebb84acdd8b16524ef
Luqmaan <syed.mohammedluqmaan@msam.christuniversity.in>
2024-12-03 00:45:56 +0530

    commit

--- JAVA LAB/Lab4.java
+++ JAVA LAB/Lab4.java
@@ -1,129 +1,137 @@
 import java.util.Scanner;
 
-abstract class Robber {
+abstract     class   Robber  
+{
 
-    
-    public void MachineLearning() {
-        System.out.println("I love Machine Learning.");
-    }
 
-    
-    public abstract void RobbingClass();
-    public abstract int RowHouses(int[] money);
-    public abstract int RoundHouses(int[] cash);
-    public abstract int SquareHouse(int[] sqHouse);
-    public abstract int MultiHouseBuilding(int[]... bldg); 
+    public void MachineLearning ( )  { 
+
+
+System.out.println(      "I love Machine Learning." )  ; }
+
+
+
+
+    public abstract void RobbingClass ( ) ; 
+public   abstract    int RowHouses ( int [ ]   money ) ; 
+    public abstract int   RoundHouses ( int[ ]   cash   ) ; 
+
+
+
+    public abstract int SquareHouse( int[]  sqHouse )   ; 
+    public   abstract int  MultiHouseBuilding ( int[ ]... bldg ) ; }
+
+
+
+class    JAVAProfessionalRobber extends Robber { 
+
+@Override 
+   public void RobbingClass( )    { 
+System.out.println("MSc AI & ML"); 
 }
 
-class JAVAProfessionalRobber extends Robber {
+private 
+    int simpleRob(int[]arr,int start, int stop){
+int robA = 0,robB = 0;
 
-    
-    @Override
-    public void RobbingClass() {
-        System.out.println("MSc AI & ML");
-    }
+        for (int i=start;i <=stop ; i++) 
+{
+int tmp   =  robB ; 
 
-    
-    private int simpleRob(int[] arr, int start, int stop) {
-        int robA = 0, robB = 0;
-        for (int i = start; i <= stop; i++) {
-            int tmp = robB;
-            robB = Math.max(robA + arr[i], robB);
-            robA = tmp;
-        }
-        return robB;
-    }
 
-    
-    @Override
-    public int RowHouses(int[] row) {
-        int rob1 = 0, rob2 = 0;
-        for (int r : row) {
-            int temp = rob2;
-            rob2 = Math.max(rob1 + r, rob2);
-            rob1 = temp;
-        }
-        return rob2;
-    }
+robB=Math.max(robA + arr[i],robB)  ; robA = tmp; 
+} 
 
-    
-    @Override
-    public int RoundHouses(int[] round) {
-        if (round.length == 1) return round[0];
-        return Math.max(simpleRob(round, 0, round.length - 2), simpleRob(round, 1, round.length - 1));
-    }
+return robB ; 
+}
+@Override 
+public int RowHouses  (int[]row)  { 
 
-    
-    @Override
-    public int SquareHouse(int[] square) {
-        return RowHouses(square);
-    }
+int rob1=0,rob2=0; for ( int r : row ) 
+{
 
-    
-    public int MultiHouseBuilding(int[]... build) {
-        int total = 0;
-        for (int[] bld : build) {
-            total += RowHouses(bld);
-        }
-        return total;
-    }
+int temp= rob2   ; 
+rob2 = Math.max (rob1+r ,rob2); 
+rob1=temp; }
+return rob2; 
+}
+@Override
+
+public  int RoundHouses(int[ ]round )   {
+
+if(round.length==1)return  round [ 0] ; 
+return Math.max ( 
+simpleRob(round,0,round.length-2) ,   simpleRob(round,1,round.length-1)  ); }
+
+@Override 
+    public int SquareHouse (int[ ]square ) 
+
+{
+
+return  RowHouses(square); 
 }
 
-public class Lab4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        JAVAProfessionalRobber robber = new JAVAProfessionalRobber();
+public  int MultiHouseBuilding ( int[ ]... build) 
+    { int total =0; 
 
-        
-        System.out.println("Welcome to the Robber Simulation Program!");
-        robber.RobbingClass();
-        robber.MachineLearning();
 
-        
-        System.out.print("Enter number of houses for RowHouses: ");
-        int n = sc.nextInt();
-        int[] rowHouses = new int[n];
-        System.out.println("Enter the money in each house:");
-        for (int i = 0; i < n; i++) {
-            rowHouses[i] = sc.nextInt();
-        }
-        System.out.println("Maximum money from RowHouses: " + robber.RowHouses(rowHouses));
+    for (int[]bld: build ) { 
+total += RowHouses (bld) ; }
+return total  ; 
+    } } 
 
-        
-        System.out.print("Enter number of houses for RoundHouses: ");
-        int m = sc.nextInt();
-        int[] roundHouses = new int[m];
-        System.out.println("Enter the money in each house:");
-        for (int i = 0; i < m; i++) {
-            roundHouses[i] = sc.nextInt();
-        }
-        System.out.println("Maximum money from RoundHouses: " + robber.RoundHouses(roundHouses));
 
-        
-        System.out.print("Enter number of houses for SquareHouse: ");
-        int p = sc.nextInt();
-        int[] squareHouse = new int[p];
-        System.out.println("Enter the money in each house:");
-        for (int i = 0; i < p; i++) {
-            squareHouse[i] = sc.nextInt();
-        }
-        System.out.println("Maximum money from SquareHouse: " + robber.SquareHouse(squareHouse));
+public class Lab4  
+{ 
+public static void main(String[ ]args)  { 
+Scanner sc = new Scanner( System.in); 
 
-        
-        System.out.print("Enter number of rows of houses for MultiHouseBuilding: ");
-        int q = sc.nextInt();
-        int[][] multiHouses = new int[q][];
-        for (int i = 0; i < q; i++) {
-            System.out.print("Enter number of houses in row " + (i + 1) + ": ");
-            int r = sc.nextInt();
-            multiHouses[i] = new int[r];
-            System.out.println("Enter the money in each house:");
-            for (int j = 0; j < r; j++) {
-                multiHouses[i][j] = sc.nextInt();
-            }
-        }
-        System.out.println("Maximum money from MultiHouseBuilding: " + robber.MultiHouseBuilding(multiHouses));
 
-        sc.close();
-    }
+JAVAProfessionalRobber   robber = new JAVAProfessionalRobber() ;
+
+System.out.println ( "Welcome to the Robber Simulation Program!" ); 
+robber.RobbingClass ( ) ; 
+robber.MachineLearning()  ; 
+System.out.print( "Enter number of houses for RowHouses: " )  ; 
+int n = sc.nextInt();int [ ]rowHouses= new int [n] ;
+System.out.println ( "Enter the money in each house: ") ; 
+for (int i=0;i<n; i++) 
+{ rowHouses [i] = sc.nextInt() ; }
+
+System.out.println("Maximum money from RowHouses: "+ 
+robber.RowHouses(rowHouses) )   ;
+
+
+System.out.print ("Enter number of houses for RoundHouses: "); 
+int m=sc.nextInt(); int []roundHouses=new int [m];
+
+System.out.println ( "Enter the money in each house:" )  ; 
+for (int i=0 ; i < m ;i++){roundHouses[i]= sc.nextInt() ; }
+System.out.println("Maximum money from RoundHouses: "+ 
+robber.RoundHouses(roundHouses));
+
+System.out.print("Enter number of houses for SquareHouse: ")   ; 
+int p = sc.nextInt();int []squareHouse =new int [p];
+System.out.println("Enter the money in each house: ");
+for (int i= 0; i< p; i++)
+{ 
+squareHouse [ i] =sc.nextInt( ); } 
+
+
+System.out.println("Maximum money from SquareHouse: "+ robber.SquareHouse(squareHouse));
+
+System.out.print("Enter number of rows of houses for MultiHouseBuilding: ") ; 
+int q=sc.nextInt( );int[][] multiHouses = new int[q][];
+for(int i=0;i<q;i++) {
+    System.out.print( "Enter number of houses in row "+ ( i+1) +": " )  ; 
+int r =sc.nextInt( ) ;
+multiHouses[i]= new int[r];
+System.out.println("Enter the money in each house: "); 
+for(int j= 0;j<r;j++) 
+{
+
+multiHouses[ i ][ j] = sc.nextInt() ;
+    }}
+System.out.println("Maximum money from MultiHouseBuilding: "+ robber.MultiHouseBuilding(multiHouses))  ; 
+sc.close( );} 
 }
--- JAVA LAB/LibraryManagementSystem.java
+++ JAVA LAB/LibraryManagementSystem.java
@@ -34,11 +34,12 @@ class Book {
         }   
         else 
         {
-                    System.out.println("Book was not borrowed: " + title);
+                           System.out.println("Book was not borrowed: " + title);
         }
     }
-    public void displayInfo() {
-                System.out.println("Title: " + title + ", Author: " + author + ", Available: " + (isAvailable ? "Yes" : "No"));
+    public void displayInfo() 
+    {
+                  System.out.println("Title: " + title + ", Author: " + author + ", Available: " + (isAvailable ? "Yes" : "No"));
     }
 }
 
@@ -47,12 +48,12 @@ class ReferenceBook extends Book {
     private int edition;
 public ReferenceBook(int bookId, String title, String author, int edition) {
             super(bookId, title, author);
-          this.edition = edition;
+        this.edition = edition;
     }
 
     public void displayInfo() {
-          super.displayInfo();
-        System.out.println("Edition: " + edition);
+                   super.displayInfo();
+    System.out.println("Edition: " + edition);
     }
 }
 
@@ -71,28 +72,29 @@ public void displayInfo() {
 
 // Main Class
 public class LibraryManagementSystem {
-    public static void main(String[] args) {
-Book book1 = new FictionBook(101, "ghost diaries", "alan john", "horror");
-Book book2 = new ReferenceBook(201, "design of algorithms", "syed ali", 3);
-Book book3 = new FictionBook(102,"horror comedies","vivek george","horror");
+    public static void main(String[] args) 
+    {
+           Book book1 = new FictionBook(101, "ghost diaries", "alan john", "horror");
+       Book book2 = new ReferenceBook(201, "design of algorithms", "syed ali", 3);
+   Book book3 = new FictionBook(102,"horror comedies","vivek george","horror");
 
         
 System.out.println("Library Inventory:");
-book1.displayInfo();
-book2.displayInfo();
-book3.displayInfo();
+    book1.displayInfo();
+       book2.displayInfo();
+       book3.displayInfo();
 
         
 System.out.println("\nBorrowing Books:");
-book2.borrowBook(); 
-book2.borrowBook(); 
+   book2.borrowBook(); 
+     book2.borrowBook(); 
 
 System.out.println("\nReturning Books:");
-        book2.returnBook(); 
+         book2.returnBook(); 
 
 
         System.out.println("\nLibrary Final Stats:");
-        book1.displayInfo();
-        book2.displayInfo();
+                book1.displayInfo();
+                    book2.displayInfo();
     }
 }
