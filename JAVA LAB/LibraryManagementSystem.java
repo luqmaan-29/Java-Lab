419e1aa2f3e70eb35c7f20e1444c5028de527478
Luqmaan <syed.mohammedluqmaan@msam.christuniversity.in>
2024-12-02 22:38:20 +0530

    commit

--- JAVA LAB/LibraryManagementSystem.java
+++ JAVA LAB/LibraryManagementSystem.java
@@ -20,15 +20,20 @@ class Book {
         {
             isAvailable = false;
               System.out.println("Book borrowed: " + title);
-        } else {
+        } else
+         {
                  System.out.println("Book is unavailable: " + title);
         }
     }
-    public void returnBook() {
-          if (!isAvailable) {
-             isAvailable = true;
+    public void returnBook() 
+    {
+    if (!isAvailable) 
+    {
+     isAvailable = true;
                 System.out.println("Book returned: " + title);
-        }   else {
+        }   
+        else 
+        {
                     System.out.println("Book was not borrowed: " + title);
         }
     }
@@ -40,15 +45,11 @@ class Book {
 // Derived Class
 class ReferenceBook extends Book {
     private int edition;
-
-    
-    public ReferenceBook(int bookId, String title, String author, int edition) {
+public ReferenceBook(int bookId, String title, String author, int edition) {
             super(bookId, title, author);
           this.edition = edition;
     }
 
-    
-    
     public void displayInfo() {
           super.displayInfo();
         System.out.println("Edition: " + edition);
@@ -58,15 +59,11 @@ class ReferenceBook extends Book {
 // Derived Class
 class FictionBook extends Book {
     private String genre;
-
-    
-    public FictionBook(int bookId, String title, String author, String genre) {
+public FictionBook(int bookId, String title, String author, String genre) {
          super(bookId, title, author);
            this.genre = genre;
     }
-
-    
-    public void displayInfo() {
+public void displayInfo() {
             super.displayInfo();
          System.out.println("Genre: " + genre);
     }
@@ -75,24 +72,22 @@ class FictionBook extends Book {
 // Main Class
 public class LibraryManagementSystem {
     public static void main(String[] args) {
-        
-        Book book1 = new FictionBook(101, "ghost diaries", "alan john", "horror");
-        Book book2 = new ReferenceBook(201, "design of algorithms", "syed ali", 3);
-        Book book3 = new FictionBook(102,"horror comedies","vivek george","horror");
+Book book1 = new FictionBook(101, "ghost diaries", "alan john", "horror");
+Book book2 = new ReferenceBook(201, "design of algorithms", "syed ali", 3);
+Book book3 = new FictionBook(102,"horror comedies","vivek george","horror");
 
         
-        System.out.println("Library Inventory:");
-        book1.displayInfo();
-        book2.displayInfo();
-        book3.displayInfo();
+System.out.println("Library Inventory:");
+book1.displayInfo();
+book2.displayInfo();
+book3.displayInfo();
 
         
-        System.out.println("\nBorrowing Books:");
-        book2.borrowBook(); 
-        book2.borrowBook(); 
+System.out.println("\nBorrowing Books:");
+book2.borrowBook(); 
+book2.borrowBook(); 
 
-        
-        System.out.println("\nReturning Books:");
+System.out.println("\nReturning Books:");
         book2.returnBook(); 
 
 
